4ddac8eb7e3286778e2b05eda98280fe33f35f5f
Fastone <1206290545@qq.com>
2019-04-26 15:19:25 +0800

    Collection 集合的交集并集 测试2

---
+++ src/test/java/com/fastone/test/ArraysTest.java
@@ -0,0 +1,8 @@
+package com.fastone.test;
+
+public class ArraysTest {
+    public static void main(String[] args) {
+        long[] oldIds = {1L, 4L, 6L};
+        long[] newIds = {1L, 4L, 9L};
+    }
+}
--- src/test/java/com/fastone/test/ListTest.java
+++ src/test/java/com/fastone/test/ListTest.java
@@ -1,30 +1,33 @@
 package com.fastone.test;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class ListTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         List<Long> ids = new ArrayList<>();
         ids.add(1L);
         ids.add(2L);
-//        ids.add(4L);
         ids.add(6L);
-
-        List<Long> ods = new ArrayList<>();
-        ods.add(1L);
-        ods.add(4L);
-
-//        ids.retainAll(ods); // 交集
-//        System.out.println(ids);
-
-        ids.removeAll(ods);  // 差集
-//        System.out.println(ids);
-        ids.addAll(ods); // 差并集
-        System.out.println(ids);
-
+        List<Long> queueIds = new ArrayList<>();
+        queueIds.add(1L);
+        queueIds.add(4L);
+        do {
+            ids.retainAll(queueIds);
+            if (ids.isEmpty()) {
+                System.out.println("所有子job全部停止，可以退出循环");
+                break;
+            } else {
+                log.info("还有{}个job没有停止", ids.size());
+                Thread.sleep(1000);
+                queueIds.remove(0);
+            }
+        } while (!ids.isEmpty());
 
     }
 
